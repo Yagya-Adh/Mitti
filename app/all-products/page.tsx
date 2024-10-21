035e762fafed25b60daf9e0a40d177070d39e291
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-21 17:59:39 +0545

    filter side bar nav bar is added

--- app/all-products/page.tsx
+++ app/all-products/page.tsx
@@ -1,15 +1,40 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import ImagePriceCard from "../components/card/ImagePriceCard";
 import MainImageBanner from "../components/banner/MainImageBanner";
 import FilterComponent from "../categories/filter/FilterComponent";
 const AllProducts = () => {
+  const [open, setOpen] = useState(false);
+
+  const filterNavBarPop = () => {
+    return setOpen(!open);
+  };
   return (
     <>
-      <MainImageBanner Title={"All Product"} />
-      <FilterComponent />
-      <ImagePriceCard varriant="image-card" />
+      <div className="bg-mitti-bodyColor relative z-10">
+        <MainImageBanner Title={"All Product"} />
+        <FilterComponent filterClick={filterNavBarPop} />
+        <ImagePriceCard varriant="image-card" />
+
+        {open && (
+          <div className="absolute  top-0 h-screen z-40">
+            <div className="bg-mitti-bodyColor h-full w-1/5 p-10">
+              <h1
+                className="flex justify-end text-xl"
+                onClick={filterNavBarPop}
+              >
+                X
+              </h1>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+              exercitationem iusto facere facilis quasi. Sed mollitia velit,
+              alias facilis quisquam consectetur culpa doloribus nostrum
+              possimus nobis, magni, quae aliquam itaque.
+            </div>
+            <div className="bg-black opacity-80 w-full"></div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- app/categories/filter/FilterComponent.tsx
+++ app/categories/filter/FilterComponent.tsx
@@ -3,32 +3,37 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React from "react";
 
-const FilterComponent = () => {
+interface IfilterProps {
+  filterClick: () => void;
+}
+const FilterComponent = ({ filterClick }: IfilterProps) => {
   return (
-    <section className="max-w-screen-2xl mx-auto px-4">
-      <div className="py-10 flex justify-between items-center">
-        {/* search */}
+    <>
+      <section className="max-w-screen-2xl mx-auto px-4">
+        <div className="py-10 flex justify-between items-center">
+          {/* search */}
 
-        <div className="flex  items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="py-3 px-3 border border-black focus:outline-none"
-          />
-          <MagnifyingGlassIcon className="size-12 bg-mitti-secondaryTextColor p-1 text-white" />
-        </div>
+          <div className="flex  items-center">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="py-3 px-3 border border-black focus:outline-none"
+            />
+            <MagnifyingGlassIcon className="size-12 bg-mitti-secondaryTextColor p-1 text-white" />
+          </div>
 
-        <div className="flex items-center">
-          <Image
-            src="/assets/icons/Filter.svg"
-            alt="filter_"
-            width={24}
-            height={24}
-          />
-          <span className="font-bold text-xl ms-2">Filter</span>
+          <div className="flex items-center" onClick={filterClick}>
+            <Image
+              src="/assets/icons/Filter.svg"
+              alt="filter_"
+              width={24}
+              height={24}
+            />
+            <span className="font-bold text-xl ms-2">Filter</span>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
