6385bb2f6e1cb8dadde2e36c0e6c9d92c503866b
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-21 14:45:57 +0545

    save changes with  data map inside single page

--- app/components/products/SingleProductDetails.tsx
+++ app/components/products/SingleProductDetails.tsx
@@ -6,9 +6,6 @@ import allproductDetails from "@/products.json";
 
 const data = allproductDetails;
 
-import React from "react";
-import { div } from "framer-motion/client";
-
 const SingleProductDetails = () => {
   return (
     <main>
@@ -18,14 +15,18 @@ const SingleProductDetails = () => {
 
           <section className="grid grid-cols-10 gap-10 items-start py-40">
             <aside className="row-span-10 col-span-6 grid grid-cols-6 items-center">
-              <div className="col-span-1 p-10">
-                <Image
-                  src={list.image}
-                  alt=""
-                  width={80}
-                  height={80}
-                  className=""
-                />
+              <div className="col-span-1 items-center flex-col justify-center px-10">
+                {list.sampleImage?.map((sample) => (
+                  <div key={sample.id} className="my-4">
+                    <Image
+                      src={sample.imageSample}
+                      alt={"sample_"}
+                      width={100}
+                      height={100}
+                      className=""
+                    />
+                  </div>
+                ))}
               </div>
               <div className="col-span-5">
                 <Image
@@ -67,7 +68,7 @@ const SingleProductDetails = () => {
               </form>
             </aside>
           </section>
-          <ProductFooterSection />
+          <ProductFooterSection listData={list.productInformation} />
         </div>
       ))}
     </main>
@@ -76,11 +77,32 @@ const SingleProductDetails = () => {
 
 export default SingleProductDetails;
 
-const ProductFooterSection = () => {
+const ProductFooterSection = ({ listData }) => {
   return (
-    <section className="max-w-screen-2xl mx-auto px-4">
+    <section className="max-w-screen-2xl mx-auto px-4 font-sans text-mitti-secondaryTextColor">
       <TabBar />
-      {/* <p>{list.description}</p> */}
+      <p>
+        {listData.describle?.map((listing) => (
+          <div key={listing.id}>{listing.slug}</div>
+        ))}
+      </p>
+
+      <SectionGroup Text="Benifita" data={listData.benefits} />
+      <SectionGroup Text="Feature" data={listData.features} />
+    </section>
+  );
+};
+
+const SectionGroup = ({ Text, data }) => {
+  return (
+    <section className="py-5">
+      <h3 className="font-bold font-serif">{Text}</h3>
+      {data?.map((list) => (
+        <p key={list.id} className="py-2">
+          {list.id}. <b> {list.title} </b>
+          {list.slug}
+        </p>
+      ))}
     </section>
   );
 };
