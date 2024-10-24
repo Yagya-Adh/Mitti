13c0cd1e011925c00e7b6c697612b3caec887938
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-24 14:20:59 +0545

    routes path is provided to data  naavlink

--- app/all-products/page.tsx
+++ app/all-products/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Link from "next/link";
 import React, { useState } from "react";
 import ImagePriceCard from "../components/card/ImagePriceCard";
 import MainImageBanner from "../components/banner/MainImageBanner";
@@ -32,21 +32,22 @@ const AllProducts = () => {
 
                 <ul className="flex flex-col py-4 leading-10">
                   {navCategoryLink?.map((list) => (
-                    <li
-                      key={list.id}
-                      className={`
+                    <Link href={list.path} key={list.id}>
+                      <li
+                        className={`
                       max-w-sm  
                       font-serif
                       text-mitti-paragraphColor     
                       cursor-pointer
                       group  
                       `}
-                    >
-                      {list.name}
-                      <div className="h-0.5 bg-transparent rounded-full">
-                        <div className="bg-mitti-paragraphColor rounded-full h-0.5 transition-all duration-300 ease-in-out w-0 group-hover:w-full"></div>
-                      </div>
-                    </li>
+                      >
+                        {list.name}
+                        <div className="h-0.5 bg-transparent rounded-full">
+                          <div className="bg-mitti-paragraphColor rounded-full h-0.5 transition-all duration-300 ease-in-out w-0 group-hover:w-full"></div>
+                        </div>
+                      </li>
+                    </Link>
                   ))}
                 </ul>
               </section>
@@ -62,10 +63,10 @@ const AllProducts = () => {
 export default AllProducts;
 
 const navCategoryLink = [
-  { id: 1, path: "", name: "All Products" },
-  { id: 2, path: "", name: "Bathroom" },
-  { id: 3, path: "", name: "Home decor" },
-  { id: 4, path: "", name: "Dining" },
-  { id: 5, path: "", name: "kitchen" },
-  { id: 6, path: "", name: "Pottery" },
+  { id: 1, path: "/all-products/", name: "All Products" },
+  { id: 2, path: "/categories/bath-room", name: "Bathroom" },
+  { id: 3, path: "/categories/home-decor", name: "Home decor" },
+  { id: 4, path: "/categories/dining", name: "Dining" },
+  { id: 5, path: "/categories/kitchen", name: "kitchen" },
+  { id: 6, path: "/categories/pottery", name: "Pottery" },
 ];
